ede87fb2f39055d4da35c62cbe69776e7d4f68db
Luc Perin Panta <lucapameni@gmail.com>
2021-08-25 11:23:45 +0100

    customizing email

--- backendEtech/settings.py
+++ backendEtech/settings.py
@@ -111,7 +111,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'backendEtech/templates')
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
