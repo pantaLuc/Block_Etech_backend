e6ef543ded4015b84c93d54156ad981fa0f51c1a
Luc Perin Panta <lucapameni@gmail.com>
2021-08-25 20:43:15 +0100

    les test

--- users/serializers.py
+++ users/serializers.py
@@ -10,12 +10,10 @@ from rolepermissions.roles import assign_role
 
 class RegisterSerializer(RegisterSerializer):
     gender = serializers.ChoiceField(choices=GENDER_SELECTION)
-    phone_number = serializers.CharField(max_length=30)
     @transaction.atomic
     def save(self, request):
         user = super().save(request)
         user.gender = self.data.get('gender')
-        user.phone_number = self.data.get('phone_number')
         user.save()
         assign_role(user ,user.role)
         user.save()
