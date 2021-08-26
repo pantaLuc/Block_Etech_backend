3cc6049c7fea74527ef50e5e1e9b19f8e296ad62
Luc Perin Panta <lucapameni@gmail.com>
2021-08-26 08:38:11 +0100

    modification

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from typing_extensions import ParamSpecArgs
+from rest_framework import viewsets
 from users.serializers import UserDetailsSerializer
 from django.shortcuts import render
 from .models import User
@@ -8,7 +10,7 @@ from allauth.socialaccount.providers.twitter.views import TwitterOAuthAdapter
 from dj_rest_auth.social_serializers import TwitterLoginSerializer
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-
+from rolepermissions.roles import assign_role , get_user_roles
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
@@ -23,4 +25,15 @@ class GithubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
     #callback_url = CALLBACK_URL_YOU_SET_ON_GITHUB
     client_class = OAuth2Client
+
+class changeUserRole(viewsets.ViewSet):
+
+    def change_user_role_to_admin(self ,request ,pk):
+        user=User.objects.get(id=pk)
+        if user.role !='user' or user.role!='writer':
+            assign_role(user ,'admin')
+            user.role='admin'
+            user.save()
+        else :
+            pass
 
