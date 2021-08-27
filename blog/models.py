6a99cd121a904239e9bc3d1e0a256de11bffa5c0
Luc Perin Panta <lucapameni@gmail.com>
2021-08-27 09:59:25 +0100

    les modifications

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
 
 
 class Tag(models.Model): 
-    user = models.ForeignKey(User ,related_name="user_tags", on_delete=models.CASCADE)
+    author  = models.ForeignKey(User ,related_name="user_tags", on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=False, unique=True)
     slug = models.SlugField(default='', editable=False, max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
